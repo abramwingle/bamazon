3cee520e5daf436653f6136d4f5f7a0238c53f93
Abram Wingle <abramwingle15@gmail.com>
2019-02-22 14:38:13 -0700

    Finished and included video explaining functionality

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -9,48 +9,60 @@ var connection = mysql.createConnection({
 });
 
 
-connection.connect(function (err) {
-    if (err) throw err;
-    console.log("connected as id " + connection.threadId);
-    afterConnection();
-});
+afterConnection();
 
 function afterConnection() {
-    connection.query("SELECT * FROM products", function (err, res) {
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                message: "Please input the product ID (1-11)",
+                choices: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"],
+                name: "productSearched"
+            },
+        
+            {
+                type: "input",
+                message: "How many would you like?",
+                name: "productCount",
+            }
+        ])
+        .then(function (inquirerResponse) {
 
-        if (err) throw err;
 
-        inquirer
-            .prompt([
-                // Here we create a basic text prompt.
+            var searchedInt = parseInt(inquirerResponse.productSearched);
+            var countInt = parseInt(inquirerResponse.productCount);
 
-                {
-                    type: "list",
-                    message: "Please input the product ID (1-11)",
-                    choices: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"],
-                    name: "productSearched"
-                },
-                // Here we ask the user to confirm.
-                {
-                    type: "input",
-                    message: "How many would you like?",
-                    name: "productCount",
-                }
-            ])
-            .then(function (inquirerResponse) {
-                var searchedInt = parseInt(inquirerResponse.productSearched);
-                var countInt = parseInt(inquirerResponse.productCount);
+            connection.query("SELECT * FROM products WHERE ?", { ID: searchedInt }, function (err, res) {
+                if (err) throw err;
+
+
                 if (isNaN(countInt)) {
-                    console.log("You must enter a number")
+                    console.log("You must enter a number.")
                     afterConnection();
                 }
                 else {
-                    console.log("great job, keep going")
+                    var specificProduct = res[0];
+
+                    if (specificProduct.stock_quantity < countInt) {
+                        console.log("There are not enough of that item.")
+                        afterConnection();
+                    }
+                    else {
+                        var updateQueryStr = "UPDATE products SET stock_quantity = " + (specificProduct.stock_quantity - countInt) + ' WHERE ID = ' + searchedInt;
+                        connection.query(updateQueryStr, function (err, data) {
+                            if (err) throw err;
+                            console.log("The price for each " + (specificProduct.product_name) + " is $" + specificProduct.price);
+                            console.log("Your total is $" + (specificProduct.price * countInt));
+                            console.log("Thank you for shopping with us!");
+                            connection.end();
+                        })
 
 
+                    }
 
-                    connection.end();
                 }
             });
-    });
+        });
 }
+
